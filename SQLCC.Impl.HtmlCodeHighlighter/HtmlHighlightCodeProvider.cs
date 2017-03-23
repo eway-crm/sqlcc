14d35b845b3201faf9665d19c91acefca081268d
Roman Štefko <roman@stefko.cz>
2017-03-23 11:50:43 +0100

    Do not stop whole process when formatter fails

--- SQLCC.Impl.HtmlCodeHighlighter/HtmlHighlightCodeProvider.cs
+++ SQLCC.Impl.HtmlCodeHighlighter/HtmlHighlightCodeProvider.cs
@@ -2,28 +2,38 @@
 using System.Text;
 using SQLCC.Core;
 using SQLCC.Core.Objects;
+using System;
 
 namespace SQLCC.Impl.HtmlCodeHighlighter
 {
-   public class HtmlHighlightCodeProvider : HighlightCodeProvider
-   {
-      private const string InsertStartTag = "<span class=\"cc\">";
-      private const string InsertEndTag = "</span>";
+    public class HtmlHighlightCodeProvider : HighlightCodeProvider
+    {
+        private const string InsertStartTag = "<span class=\"cc\">";
+        private const string InsertEndTag = "</span>";
 
-      public override string HighlightCode(string code, List<DbCodeSegment> codeSegments)
-      {
-         if (codeSegments.Count == 0)
-            return code;
-         var codeContents = new StringBuilder(code);
-         var offset = 0;
-         foreach (var lineOfCode in codeSegments)
-         {
-            codeContents.Insert(lineOfCode.StartByte + offset, InsertStartTag);
-            offset += InsertStartTag.Length;
-            codeContents.Insert(lineOfCode.EndByte + offset, InsertEndTag);
-            offset += InsertEndTag.Length;
-         }
-         return codeContents.ToString();
-      }
-   }
+        public override string HighlightCode(string code, List<DbCodeSegment> codeSegments)
+        {
+            if (codeSegments.Count == 0)
+                return code;
+            var codeContents = new StringBuilder(code);
+            var offset = 0;
+            try
+            {
+                foreach (var lineOfCode in codeSegments)
+                {
+                    codeContents.Insert(lineOfCode.StartByte + offset, InsertStartTag);
+                    offset += InsertStartTag.Length;
+                    codeContents.Insert(lineOfCode.EndByte + offset, InsertEndTag);
+                    offset += InsertEndTag.Length;
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine("Unable to highlight code in '{0}', the exception is: {1}", code, ex);
+
+                return code;
+            }
+            return codeContents.ToString();
+        }
+    }
 }
